fcecf71e0566491c1b768168fd9db0935e75acdf
Pedro Henrique <38799159+pedrophs2@users.noreply.github.com>
2021-10-31 10:48:10 -0300

    Added User Validation (NULL)

--- src/main/java/com/arpdevs/businesscook/validators/auth/LoginValidator.java
+++ src/main/java/com/arpdevs/businesscook/validators/auth/LoginValidator.java
@@ -8,6 +8,13 @@ import com.arpdevs.businesscook.validators.Validator;
 
 public class LoginValidator extends Validator<User> {
 	
+	public Optional<String> validateUser() {
+		if(object == null)
+			return Optional.of("Preencha todos os dados para efetuar login");
+		
+		return Optional.empty();
+	}
+	
 	public Optional<String> validateEmail() {
 		if(object.getEmail() == null || object.getEmail().trim().equals(""))
 			return Optional.of("Email deve ser preenchido");
